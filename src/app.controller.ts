8f2b75a7a1d3f5ac0ae0972b183d15b1c2c15a9b
sieg.alexandre@gmail.com <sieg.alexandre@gmail.com>
2023-04-25 17:00:12 +0200

    refactor(controller): remove auth login from app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,11 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private authService: AuthService) { }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
