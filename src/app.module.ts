7df6576ec327a20e27ecb7791785b4435da15a81
LeoneLal <leone.lalloue@hotmail.fr>
2023-04-26 22:11:21 +0200

    chore(env): add .env for db variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,20 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'root',
-      database: 'smartstudy',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User],
-      synchronize: true,
+      synchronize: !!process.env.DB_SYNC || false,
     }),
     AuthModule, 
     UsersModule
