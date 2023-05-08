0398dbd217ea23f389c5d9bce23f8f0db5b1119c
LeoneLal <leone.lalloue@hotmail.fr>
2023-05-08 21:34:04 +0200

    feat(signup): Add password hash

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import * as argon2 from "argon2";
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async create(user: Partial<User>): Promise<User> {
+    user.password = await argon2.hash(user.password);
     return this.usersRepository.save(user);
   }
 
   async findOne(email: string): Promise<User | undefined> {
-    return this.usersRepository.findOne({ where: {email: email} })
+    return this.usersRepository.findOne({ where: { email: email } })
   }
 
   async findOneOrCreate(user: Partial<User>) {
