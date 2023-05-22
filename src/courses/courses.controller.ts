295b745a4d115a8742cd531c6a2ee58d2fc46945
LeoneLal <leone.lalloue@hotmail.fr>
2023-05-22 22:03:32 +0200

    feat(courses):add user uuid in course creation

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -3,6 +3,7 @@ import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UpdateCourseGuard } from './guards/update-course.guard';
 
 @Controller('courses')
 export class CoursesController {
@@ -11,7 +12,6 @@ export class CoursesController {
   @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createCourseDto: CreateCourseDto, @Req() req) {
-    console.log(req.user)
     createCourseDto["user"] = req.user
     return this.coursesService.create(createCourseDto);
   }
@@ -26,6 +26,7 @@ export class CoursesController {
     return this.coursesService.findOne(uuid);
   }
 
+  @UseGuards(JwtAuthGuard, UpdateCourseGuard)
   @Patch(':uuid')
   update(@Param('uuid') uuid: string, @Body() updateCourseDto: UpdateCourseDto) {
     return this.coursesService.update(uuid, updateCourseDto);
