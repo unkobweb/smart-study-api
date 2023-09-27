e45fc273a1f78bda2a7665fc1a3435ce7beb2dcc
Alexandre Sieg <sieg.alexandre@gmail.com>
2023-09-27 20:07:27 +0200

    chore(app): add express json

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   console.log(process.env.NODE_ENV)
@@ -12,6 +13,8 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
   })
+  app.use(json({limit: '50mb'}))
+  app.use(urlencoded({limit: '50mb', extended: true}))
   app.useGlobalPipes(new ValidationPipe({transform: true}))
   app.use((req, res, next) => {
     console.log(req.method, req.url)
